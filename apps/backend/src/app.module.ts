85fcd67b135be4880b5b925206340b674ce50a40
AkiiTV <iloveanimegirls99@gmail.com>
2022-11-06 21:10:40 +0100

    feat: mongoose config

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { mongoConfig } from './config/mongo.config';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(mongoConfig.MONGO_URI as string, {
+      dbName: 'kuuki',
+      user: mongoConfig.MONGO_USER,
+      pass: mongoConfig.MONGO_PASSWORD,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
