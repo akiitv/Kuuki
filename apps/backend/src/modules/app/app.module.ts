55182d5a721bb90a81f6bed29caab507e17c1283
AkiiTV <iloveanimegirls99@gmail.com>
2022-11-06 21:15:50 +0100

    feat: move app module to seperate directory

--- apps/backend/src/modules/app/app.module.ts
+++ apps/backend/src/modules/app/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { mongoConfig } from 'src/config/mongo.config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { mongoConfig } from './config/mongo.config';
 
 @Module({
   imports: [
